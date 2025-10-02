792da92c6b064b32beb4ec213feb59b7686bbfd3
RUCcriss <150340635+RUCcriss@users.noreply.github.com>
2025-10-02 20:03:10 +0200

    Update RequestValidator.cs

--- CIT_2025_Assignment3PartI-master/CIT_2025_Assignment3PartI-master/Assignment3/RequestValidator.cs
+++ CIT_2025_Assignment3PartI-master/CIT_2025_Assignment3PartI-master/Assignment3/RequestValidator.cs
@@ -4,12 +4,14 @@ using System;
 
 namespace Assignment3
 {
+#nullable enable
+
     //request klasse
     public class Request
     {
         public string? Method { get; set; }
         public string? Path { get; set; }
-        public long Date { get; set; }
+        public long? Date { get; set; }
         public string? Body { get; set; }
 
     }
@@ -21,5 +23,58 @@ namespace Assignment3
         public string? Body { get; set; } = null;
     }
 
+    public class RequestValidator
+    {
+        private List<string> acceptedM = new List<string> { "read", "create", "echo", "delete", "update" };
 
+        public Response ValidReq(Request request)
+        {
+            List<string> Errors = new List<string>();
+
+            if (string.IsNullOrEmpty(request.Method))
+            {
+                Errors.Add("missing method");
+            }
+            else if (!acceptedM.Contains(request.Method))
+            {
+                Errors.Add("illegal method");
+            }
+
+            if (string.IsNullOrEmpty(request.Path))
+            {
+                Errors.Add("missing path");
+            }
+            if (request.Date == null)
+            {
+                Errors.Add("missing date");
+                if (request.Date > 0)
+                {
+                    Errors.Add("illegal date");
+                }
+            }
+
+
+            if (request.Method != "read")
+            {
+                if (string.IsNullOrEmpty(request.Body))
+                {
+                    Errors.Add("missing body");
+                }
+            }
+
+            if (Errors.Count > 0)
+            {
+                return new Response
+                {
+                    Status = "4 " + string.Join(", ", Errors),
+                    Body = null
+                };
+            }
+            return new Response
+            {
+                Status = "1 Ok",
+                Body = null
+            };
+        }
+    }
 }

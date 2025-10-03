72cf138d6a929368cad3983e80f70e15f6e91054
RUCcriss <150340635+RUCcriss@users.noreply.github.com>
2025-10-03 14:03:14 +0200

    Update UrlParser.cs

--- CIT_2025_Assignment3PartI-master/CIT_2025_Assignment3PartI-master/Assignment3/UrlParser.cs
+++ CIT_2025_Assignment3PartI-master/CIT_2025_Assignment3PartI-master/Assignment3/UrlParser.cs
@@ -1,10 +1,22 @@
-using System.Collections.Generic;
-using System.Linq;
-using System;
-
 namespace Assignment3
 {
 
+    public class UrlParser
+    {
+        public bool HasId { get; set; }
+        public string Id { get; set; }
+        public string Path { get; set; }
 
+        public UrlParser()
+        {
+            HasId = false;
+            Id = string.Empty;
+            Path = string.Empty;
+        }
 
+        public static bool ParseUrl(string url)
+        {
+            return true;
+        }
+    }
 }
